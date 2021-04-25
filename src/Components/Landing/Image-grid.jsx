a9c3e4c12819dabc23365f10336bd3f6ca59f64d
Chandan <chandan.b@pacewisdom>
2021-04-25 21:43:49 +0530

    added scroll animation with layered pinning effect

--- src/Components/Landing/Image-grid.jsx
+++ src/Components/Landing/Image-grid.jsx
@@ -7,6 +7,24 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import Renderimagedata from "../../Data/Image-data";
 
 const Renderimagegrid = () => {
+  useEffect(() => {
+    const gsapTimeline = gsap.timeline();
+    const imgAnimation = ".img-animation";
+    gsapTimeline.from(imgAnimation, {
+      ease: Power2.easeInOut,
+      y: 100,
+      opacity: 0,
+      duration: 0.6,
+      delay: 0.5,
+      stagger: 0.1,
+      scrollTrigger: {
+        trigger: imgAnimation,
+        start: "top 65%",
+        end: "+=100",
+        scrub: 1,
+      },
+    });
+  }, []);
   return (
     <section className="image-grid panel">
       <Container>
@@ -14,7 +32,7 @@ const Renderimagegrid = () => {
           {Renderimagedata.map((getImageData) => {
             return (
               <Col md={3} key={getImageData.imgGrid}>
-                <div className="img-wrapper">
+                <div className="img-wrapper img-animation">
                   <img src={getImageData.imgGrid} className="img-fluid" />
                 </div>
               </Col>
--- src/Master.jsx
+++ src/Master.jsx
@@ -2,8 +2,34 @@ import React, { useEffect } from "react";
 import Renderlandingsection from "./Components/Landing/Index";
 import Scrollgif from "./assets/images/scroll-down.gif";
 import Renderimagegrid from "./Components/Landing/Image-grid";
+import gsap, { Power2 } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const Rendermastercomponent = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const ST = ScrollTrigger;
+    const targetPanels = ".panel";
+    console.log(targetPanels);
+    /* using the gsap utils menthod to loop through each of the sections and apply the layered pinning animation */
+    gsap.utils.toArray(targetPanels).map((panel) => {
+      ST.create({
+        trigger: panel,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+        scrub: 3,
+        /* use snapping to snap the sections to the closet point */
+        snap: {
+          snapTo: 1,
+          duration: { min: 0.3, max: 0.7 },
+          ease: "power2.inOut",
+          anticipatePin: 0.2,
+          delay: 0,
+        },
+      });
+    });
+  }, []);
   return (
     <>
       <Renderlandingsection Scrollgif={Scrollgif} />
