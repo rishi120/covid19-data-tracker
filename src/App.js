c89a8cfd84c70a6090584bf085b0682a9a4a4e38
Chandan <chandan.b@pacewisdom>
2021-04-25 20:00:24 +0530

    created the setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Rendermastercomponent from "./Master";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Rendermastercomponent />;
+}
+
+export default App;
---
+++ src/Master.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const Rendermastercomponent = () => {
+  return <h1>Hello World!</h1>;
+};
+
+export default Rendermastercomponent;
